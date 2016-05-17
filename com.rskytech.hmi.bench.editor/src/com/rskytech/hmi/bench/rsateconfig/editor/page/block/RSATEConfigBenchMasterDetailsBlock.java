526d174fc3231af5d922830c4e51a8ea2e5e9cbd
robin <robin@localhost.localdomain>
2016-05-18 00:00:40 +0800

    :)

--- com.rskytech.hmi.bench.editor/src/com/rskytech/hmi/bench/rsateconfig/editor/page/block/RSATEConfigBenchMasterDetailsBlock.java
+++ com.rskytech.hmi.bench.editor/src/com/rskytech/hmi/bench/rsateconfig/editor/page/block/RSATEConfigBenchMasterDetailsBlock.java
@@ -14,6 +14,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.ui.forms.DetailsPart;
 import org.eclipse.ui.forms.IManagedForm;
@@ -33,38 +34,48 @@ import com.rskytech.hmi.common.editor.page.block.AbstractRskyCommonMasterDetails
  * 硬件资源节点概览
  * 
  * @author robin
- *
+ * 
  */
-public class RSATEConfigBenchMasterDetailsBlock extends AbstractRskyCommonMasterDetailsBlock {
+public class RSATEConfigBenchMasterDetailsBlock extends
+		AbstractRskyCommonMasterDetailsBlock {
 
 	RSATEConfigDetailPageProvider rsATEConfigDetailPageProvider;
 
-	public RSATEConfigBenchMasterDetailsBlock(IRskyCommonFormPage rskyCommonFormPage) {
+	public RSATEConfigBenchMasterDetailsBlock(
+			IRskyCommonFormPage rskyCommonFormPage) {
 		super(rskyCommonFormPage);
 		this.rsATEConfigDetailPageProvider = new RSATEConfigDetailPageProvider();
 	}
 
 	@Override
-	protected void createMasterPart(final IManagedForm managedForm, Composite parent) {
+	protected void createMasterPart(final IManagedForm managedForm,
+			Composite parent) {
 		FormToolkit formToolkit = managedForm.getToolkit();
-		Section section = formToolkit.createSection(parent, Section.EXPANDED | Section.TITLE_BAR);
+		Section section = formToolkit.createSection(parent, Section.EXPANDED
+				| Section.TITLE_BAR);
 		section.setText("硬件资源列表");
 		section.clientVerticalSpacing = 0;
 		section.marginWidth = 5;
 		section.marginHeight = 5;
 
-		Composite composite = formToolkit.createComposite(section, SWT.BORDER);
-		GridLayout gridLayout = new GridLayout(1, false);
-		gridLayout.marginWidth = 0;
-		gridLayout.marginHeight = 0;
+		Composite composite = formToolkit.createComposite(section, SWT.WRAP);
+		GridLayout gridLayout = new GridLayout();
+		//gridLayout.marginWidth = 0;
+		gridLayout.marginHeight = 2;
 		// gridLayout.verticalSpacing = 0;
 		// gridLayout.horizontalSpacing = 0;
 		composite.setLayout(gridLayout);
+		formToolkit.paintBordersFor(composite);
+		// --搜索
+		final Text searchText = formToolkit.createText(composite, "",
+				SWT.BORDER | SWT.SEARCH | SWT.ICON_SEARCH | SWT.CANCEL);
+		searchText.setMessage("搜索: 资源");
+		searchText.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
 
-		Tree tree = new Tree(composite, SWT.FULL_SELECTION);
+		Tree tree = formToolkit.createTree(composite, SWT.FULL_SELECTION);
 		GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, true);
-		gridData.widthHint = SWT.DEFAULT;
-		gridData.heightHint = SWT.DEFAULT;
+		gridData.widthHint = 20;
+		gridData.heightHint = 20;
 		tree.setLayoutData(gridData);
 
 		TreeViewer treeViewer = new TreeViewer(tree);
@@ -83,7 +94,8 @@ public class RSATEConfigBenchMasterDetailsBlock extends AbstractRskyCommonMaster
 		treeViewer.addSelectionChangedListener(new ISelectionChangedListener() {
 			@Override
 			public void selectionChanged(SelectionChangedEvent event) {
-				managedForm.fireSelectionChanged(sectionPart, event.getSelection());
+				managedForm.fireSelectionChanged(sectionPart,
+						event.getSelection());
 
 			}
 		});
